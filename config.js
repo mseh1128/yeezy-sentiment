7159305f2539316142ec327c6145ffbaa5958b61
Manav Sehgal <manavsehgal1128@gmail.com>
2018-07-05 10:24:20 -0400

    Add configuration file

---
+++ config.js
@@ -0,0 +1,16 @@
+let config = {};
+
+config.newsApi = {
+  searchTerm: "Kanye West",
+  numOfResults: 100, // newsAPI has max of 100, default of 20
+  numOfWeeks: 10,
+  uri: "mongodb://localhost/yeezy"
+}
+
+config.smtp = {
+    GMAIL_USER: process.env.GMAIL_USER || 'username',
+    GMAIL_PASS: process.env.GMAIL_PASS || 'password'
+}
+
+
+module.exports = config;
